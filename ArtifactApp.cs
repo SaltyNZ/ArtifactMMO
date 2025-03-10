606660566ef62164f6ddaa5603bb533149047931
Adam Macintyre <adamjmacintyre@gmail.com>
2025-03-11 02:50:11 +1300

    Redo the StartUp and UI
    Use Spectre.Console to create a new and more interactive and expandable.

--- ArtifactApp.cs
+++ ArtifactApp.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Spectre.Console;
+using Spectre.Console.Json;
 
     /*
         2.0 Rewrite
@@ -22,11 +24,60 @@ namespace ArtifactMMO
             // ----------------------
             //    Define Variables
             // ----------------------
-
-            string? characterName = "SaltyNZ";
-
             
-        }
+            //Variables
+            string token = Environment.GetEnvironmentVariable("ArtifactAPIKey") ?? "NoToken";
 
+            //Classes
+            ArtifactApiService api = new ArtifactApiService();
+            UI ui = new UI();
+            AutomationScripts auto = new AutomationScripts();
+
+
+            // -------------------
+            //    Start up Menu
+            // -------------------
+            var characterName = AnsiConsole.Prompt(
+            new TextPrompt<string>("What's your [red]character's[/] name?"));
+            AnsiConsole.WriteLine($"Character name is: {characterName}");
+
+            var uiChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[blue]Select what command to use: :computer_disk:[/]")
+                    .PageSize(10)
+                    .MoreChoicesText("[blue](Move up and down to see more selections)[/]")
+                    .AddChoices(new[] {
+                        "Character Info","Auto Ingot",
+                        "Auto Attack","Auto Plank",
+                        "Auto Task","Exit"
+                }));
+
+            switch (uiChoice)
+            {
+                case "Character Info":
+                    await api.CharacterInfoUIAsync(characterName ?? "", token);
+                    break;
+                
+                case "Auto Ingot":
+                    AnsiConsole.WriteLine($"You selected {uiChoice}");
+                    break;
+
+                case "Auto Attack":
+                    AnsiConsole.WriteLine($"You selected {uiChoice}");
+                    break;
+
+                case "Auto Plank":
+                    AnsiConsole.WriteLine($"You selected {uiChoice}");
+                    break;
+                
+                case "Auto Task":
+                    AnsiConsole.WriteLine($"You selected {uiChoice}");
+                    break;
+
+                case "Exit":
+                    AnsiConsole.WriteLine($"You selected {uiChoice}");
+                    break;
+            }   
+        }
     }
 }
